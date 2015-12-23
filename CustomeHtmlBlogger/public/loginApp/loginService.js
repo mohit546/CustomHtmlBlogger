9c1e32ad92acd703e2fe6f63ab00a1f57b5721e5
Mohit Patil <mohitpatil546@gmail.com>
2015-12-23 23:23:47 +0530

    login and signup services added

--- CustomeHtmlBlogger/public/loginApp/loginService.js
+++ CustomeHtmlBlogger/public/loginApp/loginService.js
@@ -1,4 +1,23 @@
 angular.module('loginApp.service', [])
 .factory('loginService', function($http){
 
+
+	var signUpRequest = function(useremail,password1, password2){
+		return $http.post('/api/signup/', {userEmail: useremail, password1: password1, password2: password2}).then(function(result){
+			return result.data;
+		});
+	};
+
+	var loginRequest = function(useremail, password){
+		return $http.post('/api/login/', {userEmail: useremail, password: password}).then(function(result){
+			return result.data;
+		});
+	};
+
+
+	return {
+		signUpRequest: signUpRequest,
+		loginRequest : loginRequest
+	};
+
 });
